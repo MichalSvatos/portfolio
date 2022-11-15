f0d700448819f635aec9d1dad9ec4f9437fd9a8f
Michal Svatoš <michal.svatos@gmail.com>
2022-11-15 09:57:37 -0500

    DeLorean hidden on mobiles + javascript disabled (performance)

--- src/components/DeLorean/DeLorean.js
+++ src/components/DeLorean/DeLorean.js
@@ -4,6 +4,7 @@ import DeLoreanTimeVariant from "./DeLoreanTimeVariant";
 
 export default function DeLorean({timePeriods}) {
 	const {present, past, history} = timePeriods
+	const deLoreanVisibilityTreshold = 575
 
 	// Make clip slices same height as sections to make the time travel "seamless"
 	const makePreciseSlices = () => {
@@ -22,52 +23,59 @@ export default function DeLorean({timePeriods}) {
 	let scrollPos = 0
 
 	window.addEventListener('scroll', () => {
-		const deLoreanContainers = document.querySelectorAll(".delorean__container")
-		const deLoreanLandingThreshold = document.querySelector(".time--present").offsetHeight - (window.innerHeight / 2)
+		if (window.innerWidth > deLoreanVisibilityTreshold) {
 
-		if ((document.body.getBoundingClientRect()).top > scrollPos) {
-			// -- when you go BACK TO THE FUTURE!
-			deLoreanContainers.forEach(delorean => {
-				delorean.classList.add('is-going-back')
-			})
+			const deLoreanContainers = document.querySelectorAll(".delorean__container")
+			const deLoreanLandingThreshold = document.querySelector(".time--present").offsetHeight - (window.innerHeight / 2)
 
-			// -- “A flying DeLorean? I haven’t seen one of those in 30 years”
-			if (window.scrollY < (deLoreanLandingThreshold + 350)) {
-				document.querySelector(".delorean__container--past").classList.add("is-hovering")
+			if ((document.body.getBoundingClientRect()).top > scrollPos) {
+				// -- when you go BACK TO THE FUTURE!
+				deLoreanContainers.forEach(delorean => {
+					delorean.classList.add('is-going-back')
+				})
+
+				// -- “A flying DeLorean? I haven’t seen one of those in 30 years”
+				if (window.scrollY < (deLoreanLandingThreshold + 350)) {
+					document.querySelector(".delorean__container--past").classList.add("is-hovering")
+				}
+
+				if (window.scrollY < ((window.innerHeight / 2) + 10)) {
+					document.querySelector(".delorean__container--present").classList.remove("started")
+				}
+
+			} else {
+				// -- when you go back in time
+
+				// -- turn Deloreans back to facing dowm
+				deLoreanContainers.forEach(delorean => {
+					delorean.classList.remove('is-going-back')
+				})
+
+				// -- fly the Delorean to the middle of the screen
+				if (window.scrollY > 100) {
+					document.querySelector(".delorean__container--present").classList.add("started", "is-hovering")
+				}
+
+				// -- after timetravel, land Delorean
+				if (window.scrollY > deLoreanLandingThreshold) {
+					document.querySelector(".delorean__container--past").classList.remove("is-hovering")
+				}
+
 			}
 
-			if (window.scrollY < ((window.innerHeight / 2) + 10)) {
-				document.querySelector(".delorean__container--present").classList.remove("started")
-			}
-
-		} else {
-			// -- when you go back in time
-
-			// -- turn Deloreans back to facing dowm
-			deLoreanContainers.forEach(delorean => {
-				delorean.classList.remove('is-going-back')
-			})
-
-			// -- fly the Delorean to the middle of the screen
-			if (window.scrollY > 100) {
-				document.querySelector(".delorean__container--present").classList.add("started", "is-hovering")
-			}
-
-			// -- after timetravel, land Delorean
-			if (window.scrollY > deLoreanLandingThreshold) {
-				document.querySelector(".delorean__container--past").classList.remove("is-hovering")
-			}
-
+			scrollPos = (document.body.getBoundingClientRect()).top
 		}
-
-		scrollPos = (document.body.getBoundingClientRect()).top
 	})
 
 	useEffect(() => {
-		makePreciseSlices()
+		if (window.innerWidth > deLoreanVisibilityTreshold) {
+			makePreciseSlices()
+		}
 
 		window.addEventListener("resize", function () {
-			makePreciseSlices()
+			if (window.innerWidth > deLoreanVisibilityTreshold) {
+				makePreciseSlices()
+			}
 		});
 	})
 
