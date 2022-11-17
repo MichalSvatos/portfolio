a3863764c14313a160500a545522e63957d4230f
Michal Svatoš <michal.svatos@gmail.com>
2022-11-17 17:47:13 -0500

    PRESENT - Nice to have - rain (rough code)

--- src/components/Time/Time.js
+++ src/components/Time/Time.js
@@ -91,6 +91,35 @@ export default function Time({children, timePeriod = 'present', customClass = ''
 			makeTimeTravelPossible(deLoreanContainer)
 			parallax(parallaxElements, sectionPresentHeight)
 		})
+
+		// TODO: [NTH] - rain js
+		/*const makeItRain = function() {
+			//clear out everything
+			document.querySelector(".rain").innerHTML = ""
+
+			let increment = 0;
+			let drops = "";
+			let backDrops = "";
+
+			while (increment < 100) {
+				//couple random numbers to use for various randomizations
+				//random number between 98 and 1
+				let randoHundo = (Math.floor(Math.random() * (98 - 1 + 1) + 1));
+				//random number between 5 and 2
+				let randoFiver = (Math.floor(Math.random() * (5 - 2 + 1) + 2));
+				//increment
+				increment += randoFiver;
+				//add in a new raindrop with various randomizations to certain CSS properties
+				drops += '<div class="drop" style="left: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.6' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.6' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.6' + randoHundo + 's;"></div></div>';
+				backDrops += '<div class="drop" style="right: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.6' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.6' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.6' + randoHundo + 's;"></div></div>';
+			}
+
+			document.querySelector(".rain.front-row").innerHTML = drops;
+			document.querySelector(".rain.back-row").innerHTML = backDrops;
+		}
+
+		makeItRain();
+		*/
 	})
 
 	return (
@@ -98,6 +127,8 @@ export default function Time({children, timePeriod = 'present', customClass = ''
 			<div className={`time time--${timePeriod} ${customClass}`}>
 				{timePeriod === "present" ?
 					<>
+						<div className="rain front-row"></div>
+						<div className="rain back-row"></div>
 						<div className="floats">
 							{children}
 							{/* Clouds */}
