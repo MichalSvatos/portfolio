1a61db94c8e1675efec2b0b779aa8d7550dd6a48
Michal Svatoš <michal.svatos@gmail.com>
2022-11-17 17:27:51 -0500

    PRESENT - floating stuff

--- src/components/Time/Time.js
+++ src/components/Time/Time.js
@@ -1,7 +1,8 @@
 import React, {useEffect} from "react"
 import "./_time.scss"
+import "./_time--present.scss"
 
-export default function Time({ children, timePeriod = 'present', customClass = ''}) {
+export default function Time({children, timePeriod = 'present', customClass = ''}) {
 
 	// Time travel (collision detection)
 	// Thanks Chris Courses - https://yewtu.be/watch?v=_MyPLZSGS3s
@@ -50,19 +51,78 @@ export default function Time({ children, timePeriod = 'present', customClass = '
 		}
 	}
 
+	// Primitive parallax effect for the clouds
+	const parallax = (parallaxElements, sectionPresentHeight) => {
+		// -- float animation just for the bigger screens
+		if (window.innerWidth < 992) return
+
+		if ('undefined' !== parallaxElements && parallaxElements.length > 0) {
+			parallaxElements.forEach(float => {
+				let threshold = sectionPresentHeight.offsetHeight
+				let speedYBase = parseFloat(float.dataset.parallaxSpeedY)
+				let speedXBase = parseFloat(float.dataset.parallaxSpeedX)
+				let scrollY = window.scrollY
+
+				if ((scrollY > 0) && (scrollY < (threshold + (window.innerHeight / 2)))) {
+					let speedY = (speedYBase / 10) * scrollY
+					let speedX = (speedXBase / 10) * scrollY
+
+					float.style.transform = `translate3d(${speedX}px, ${speedY}px ,0)`
+
+					if (float.classList.contains("fade-out")) {
+						float.classList.remove("fade-out")
+					}
+
+				} else if (scrollY > (threshold + (window.innerHeight / 2))) {
+					float.classList.add("fade-out")
+				}
+			})
+		}
+	}
+
 	useEffect(() => {
 		const deLoreanContainer = document.querySelector(".delorean__container--present")
-		// console.log('deLoreanContainer', deLoreanContainer);
+		const sectionPresentHeight = document.querySelector(".section--present")
+
+		const parallaxElements = document.querySelectorAll('.make-me-parallax')
+		parallax(parallaxElements, sectionPresentHeight)
 
 		window.addEventListener("scroll", () => {
 			makeTimeTravelPossible(deLoreanContainer)
-		});
+			parallax(parallaxElements, sectionPresentHeight)
+		})
 	})
 
 	return (
 		<>
 			<div className={`time time--${timePeriod} ${customClass}`}>
-				{children}
+				{timePeriod === "present" ?
+					<>
+						<div className="floats">
+							{children}
+							{/* Clouds */}
+							<div className="float float--cloud make-me-parallax" data-parallax-speed-y="-2" data-parallax-speed-x="-0.15"></div>
+							<div className="float float--cloud-2 make-me-parallax" data-parallax-speed-y="-3" data-parallax-speed-x="0.15"></div>
+							<div className="float float--cloud-3 make-me-parallax" data-parallax-speed-y="-1" data-parallax-speed-x="0"></div>
+							<div className="float float--cloud-4 make-me-parallax" data-parallax-speed-y="-2" data-parallax-speed-x="-0.15"></div>
+							<div className="float float--bottom"></div>
+
+							{/* Buoys */}
+							<div className="float-wrapper float-wrapper--buoys">
+								<div className="float float--buoy make-me-parallax" data-parallax-speed-y="-3.25" data-parallax-speed-x="0.5"></div>
+								<div className="float float--buoy-2 make-me-parallax" data-parallax-speed-y="-2" data-parallax-speed-x="0.2"></div>
+								<div className="float float--buoy-3 make-me-parallax" data-parallax-speed-y="-1.5" data-parallax-speed-x="0"></div>
+								<div className="float float--buoy-4 make-me-parallax" data-parallax-speed-y="-0.95" data-parallax-speed-x="0"></div>
+								<div className="float float--buoy-5 make-me-parallax" data-parallax-speed-y="-0.65" data-parallax-speed-x="-0.05"></div>
+								<div className="float float--buoy-6 make-me-parallax" data-parallax-speed-y="-0.5" data-parallax-speed-x="-0.02"></div>
+							</div>
+						</div>
+					</>
+					:
+					<>
+						{children}
+					</>
+				}
 			</div>
 		</>
 	)
