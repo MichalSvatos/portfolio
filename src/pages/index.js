c8e27a78c3ed85011696924fe28827b23ff95bd1
Michal Svatoš <michal.svatos@gmail.com>
2022-11-20 18:17:59 -0500

    Cleaning …

--- src/pages/index.js
+++ src/pages/index.js
@@ -48,38 +48,14 @@ export default function Homepage({data}) {
 
 			<Time timePeriod={present}>
 				<Intro />
-				{/*<h1>This is heavy, doc</h1>
-				<p>Get your meat hooks off of me. Marty you gotta come back with me. But, what are you blind McFly, it's there. How else do you explain that wreck out there? Mom, is that you? Doc, she's beautiful. She's crazy about me. Look at this, look what she wrote me, Doc. That says it all. Doc, you're my only hope.</p>
-				<h2>You wanna a Pepsi, pall, you're gonna pay for it.</h2>
-				<p>Oh. I'll call you tonight. George McFly? Oh, he's kinda cute and all, but, well, I think a man should be strong, so he could stand up for himself, and protect the woman he loves. Don't you? I think it's terrible. Girls chasing boys. When I was your age I never chased a boy, or called a boy, or sat in a parked car with a boy. Next, please.</p>
-				<h3>Whoa, whoa, Biff, what's that?</h3>
-				<p>That's right, he's gonna be mayor. You okay, is everything alright? Look at the time, you've got less than 4 minutes, please hurry. Hey, hey, Doc, where are you? Quiet down, I'm sure the car is fine.</p>
-				<h4>Oh, you make it sound so easy.</h4>
-				<p>It's cold, damn cold. Ha, ha, ha, Einstein, you little devil. Einstein's clock is exactly one minute behind mine, it's still ticking. Well just gimme something without any sugar in it, okay? No no no, Doc, I just got here, okay, Jennifer's here, we're gonna take the new truck for a spin. Damn. I'm late for school. Hey man, the dance is over. Unless you know someone else who could play the guitar.</p>*/}
 				<Projects projectsData={projectSorter("present")} />
 			</Time>
 
 			<Time timePeriod={past} customClass="js-time-travel-checking">
-				{/*<h1>This is heavy, doc</h1>
-				<p>Get your meat hooks off of me. Marty you gotta come back with me. But, what are you blind McFly, it's there. How else do you explain that wreck out there? Mom, is that you? Doc, she's beautiful. She's crazy about me. Look at this, look what she wrote me, Doc. That says it all. Doc, you're my only hope.</p>
-				<h2>You wanna a Pepsi, pall, you're gonna pay for it.</h2>
-				<p>Oh. I'll call you tonight. George McFly? Oh, he's kinda cute and all, but, well, I think a man should be strong, so he could stand up for himself, and protect the woman he loves. Don't you? I think it's terrible. Girls chasing boys. When I was your age I never chased a boy, or called a boy, or sat in a parked car with a boy. Next, please.</p>
-				<h3>Whoa, whoa, Biff, what's that?</h3>
-				<p>That's right, he's gonna be mayor. You okay, is everything alright? Look at the time, you've got less than 4 minutes, please hurry. Hey, hey, Doc, where are you? Quiet down, I'm sure the car is fine.</p>
-				<h4>Oh, you make it sound so easy.</h4>
-				<p>It's cold, damn cold. Ha, ha, ha, Einstein, you little devil. Einstein's clock is exactly one minute behind mine, it's still ticking. Well just gimme something without any sugar in it, okay? No no no, Doc, I just got here, okay, Jennifer's here, we're gonna take the new truck for a spin. Damn. I'm late for school. Hey man, the dance is over. Unless you know someone else who could play the guitar.</p>*/}
 				<Projects projectsData={projectSorter("past")} />
 			</Time>
 
 			<Time timePeriod={history}>
-				{/*<h1>This is heavy, doc</h1>
-				<p>Get your meat hooks off of me. Marty you gotta come back with me. But, what are you blind McFly, it's there. How else do you explain that wreck out there? Mom, is that you? Doc, she's beautiful. She's crazy about me. Look at this, look what she wrote me, Doc. That says it all. Doc, you're my only hope.</p>
-				<h2>You wanna a Pepsi, pall, you're gonna pay for it.</h2>
-				<p>Oh. I'll call you tonight. George McFly? Oh, he's kinda cute and all, but, well, I think a man should be strong, so he could stand up for himself, and protect the woman he loves. Don't you? I think it's terrible. Girls chasing boys. When I was your age I never chased a boy, or called a boy, or sat in a parked car with a boy. Next, please.</p>
-				<h3>Whoa, whoa, Biff, what's that?</h3>
-				<p>That's right, he's gonna be mayor. You okay, is everything alright? Look at the time, you've got less than 4 minutes, please hurry. Hey, hey, Doc, where are you? Quiet down, I'm sure the car is fine.</p>
-				<h4>Oh, you make it sound so easy.</h4>
-				<p>It's cold, damn cold. Ha, ha, ha, Einstein, you little devil. Einstein's clock is exactly one minute behind mine, it's still ticking. Well just gimme something without any sugar in it, okay? No no no, Doc, I just got here, okay, Jennifer's here, we're gonna take the new truck for a spin. Damn. I'm late for school. Hey man, the dance is over. Unless you know someone else who could play the guitar.</p>*/}
 				<Projects projectsData={projectSorter("history")} />
 				{/*TODO: Software section */}
 				{/*<Software />*/}
