54ea73bfc307711f47881bb5b1734351ba8c9025
Michal Svatoš <michal.svatos@gmail.com>
2022-11-26 09:59:21 -0500

    Removed unused navbar (real one)

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,10 @@
 import React from "react"
 import "./_layout.scss"
-import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
 
 export default function Layout({ children }) {
 	return (
 		<>
-			<Navbar />
 			<main id="main">{children}</main>
 			<Footer />
 		</>
