896f805da34c3151641d116d9f60e79192994b46
Michal Svatos <github@svatos.dev>
2025-01-25 19:14:28 -0500

    FIX - It's a secret - no peeking! :)

---
+++ src/components/Achievements/Achievements.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from "react"
+// import "./_achievements.scss"
+
+const Achievements = () => {
+	return (
+		<>
+			<a href="">aa</a>
+		</>
+	)
+}
+
+export default Achievements;
